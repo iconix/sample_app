cb21afbe3d80deda5cdd1cd4c88a96f3a6cf5c0a
iconix <narhodes1@gmail.com>
2013-03-16 23:14:53 -0700

    Finish user edit, update, index, and destroy actions, WITHOUT rspec testing

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,8 +19,21 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def sign_out
     self.current_user = nil
     session[:current_user_id] = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
   end
 end
