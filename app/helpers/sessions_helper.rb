2007d13aabf77506e543e0e3cc815a1d55a74fc6
iconix <narhodes1@gmail.com>
2013-03-16 17:06:54 -0700

    Finish sign in, WITHOUT rspec testing or persistent sessions or section 8.3 (Cucumber) as in tutorial

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,26 @@
+module SessionsHelper
+
+  def sign_in(user)
+    session[:current_user_id] = user.id
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    if session[:current_user_id]
+      @current_user ||= User.find(session[:current_user_id])
+    end
+  end
+
+  def sign_out
+    self.current_user = nil
+    session[:current_user_id] = nil
+  end
+end
